ca231449dea67cc3fb01084d803d736e2744e995
Kasper Daelemans <daelemans.kasper@gmail.com>
2024-11-27 16:08:26 +0100

    add index, login & signup

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["username"])) {
+        // De gebruiker is niet ingelogd, dus doorsturen naar de loginpagina
+        header("location: login.php");
+        exit;
+    }
+    
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <a href="login.php">Login</a>
+</body>
+</html>
