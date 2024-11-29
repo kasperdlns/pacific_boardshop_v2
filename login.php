afd9806c8ebdbd0558031a36110db43fee12c4d8
Kasper Daelemans <daelemans.kasper@gmail.com>
2024-11-29 16:15:16 +0100

    css

--- login.php
+++ login.php
@@ -29,6 +29,7 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <form action="" method="post">
--- signup.php
+++ signup.php
@@ -32,6 +32,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log in</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
